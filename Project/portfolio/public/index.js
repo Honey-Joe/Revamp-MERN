4ecf0c204a9c56105f28c39f73752e28d6857273
Honey-Joe <honeyjoe943@gmail.com>
2024-06-14 13:50:18 +0530

    update

--- Project/portfolio/public/index.js
+++ Project/portfolio/public/index.js
@@ -9,38 +9,38 @@ var e_msg=document.getElementById("error-msg")
 
 
 
-const saveTodb = (name_form,email) =>{
-    
+const saveTodb = (name_form,email,msg) =>{
+    console.log(name_form,email,msg)
     const firebaseConfig = {
-        apiKey: "AIzaSyCkwrvla7M8opITVLhdrEA5HQ5rz97GhJ0",
-        authDomain: "revamp-mern.firebaseapp.com",
-        databaseURL: "https://revamp-mern-default-rtdb.asia-southeast1.firebasedatabase.app",
-        projectId: "revamp-mern",
-        storageBucket: "revamp-mern.appspot.com",
-        messagingSenderId: "405374174851",
-        appId: "1:405374174851:web:697757b5d20c4145ec7db6"
-      };
-      firebase.initializeApp(firebaseConfig);
+      apiKey: "AIzaSyCkwrvla7M8opITVLhdrEA5HQ5rz97GhJ0",
+      authDomain: "revamp-mern.firebaseapp.com",
+      databaseURL: "https://revamp-mern-default-rtdb.asia-southeast1.firebasedatabase.app",
+      projectId: "revamp-mern",
+      storageBucket: "revamp-mern.appspot.com",
+      messagingSenderId: "405374174851",
+      appId: "1:405374174851:web:697757b5d20c4145ec7db6"
+    };
 
-    firebase.initializeApp(firebaseConfig);
+    // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-    const db = firebase.database().ref("ReqForm");
-     
-    var newRef = db.push();
-    
-    newRef.set(
-      {
-        name_form : name_form,
-        email : email,
-        msg : msg
-      }
-    );
+const db = firebase.database().ref("ReqForm");
+ 
+var newRef = db.push();
+
+newRef.set(
+  {
+    name_form : name_form,
+    email : email,
+    msg: msg
+  }
+);
     
       
 }
  
 form.addEventListener("submit", (e) => {
     e.preventDefault()
-    saveTodb(name_form.value,email.value)
-    form.reset()
+    saveTodb(name_form.value,email.value,msg.value)
+
 })
